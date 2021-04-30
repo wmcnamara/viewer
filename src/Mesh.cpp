7d57268ac062f029e68a48f408deb8314dc8164d
Weston McNamara <westonfmcnamara@gmail.com>
2021-04-29 21:20:08 -0400

    Removed EBO generation

--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -2,16 +2,10 @@
 
 Mesh::Mesh(
 	float* vertices,
-	unsigned int* indices,
-	size_t numOfVertices,
-	size_t numOfIndices,
-	bool useEBO) :
+	size_t numOfVertices) :
 
 	m_vertices(vertices),
-	m_indices(indices),
-	m_numOfVertices(numOfVertices),
-	m_numOfIndices(numOfIndices),
-	m_useEBO(useEBO)
+	m_numOfVertices(numOfVertices)
 {		
 	//Setup buffers
 	glGenVertexArrays(1, &VAO);
@@ -19,18 +13,11 @@ Mesh::Mesh(
 
 	glBindVertexArray(VAO);
 
-	if (useEBO) 
-	{
-		glGenBuffers(1, &EBO);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices) * numOfIndices, indices, GL_STATIC_DRAW);
-	}
-
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) * numOfVertices, vertices, GL_STATIC_DRAW);
 
 	//Vertex Attributes
-	/////////////////////////////////////////////
+
 	//Positon
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
@@ -38,7 +25,6 @@ Mesh::Mesh(
 	//Texture coords
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
-	/////////////////////////////////////////////
 
 	glEnable(GL_DEPTH_TEST);
 }
--- src/Mesh.h
+++ src/Mesh.h
@@ -1,41 +1,20 @@
 #pragma once
 #include "glad/glad.h"
-#include "stb_image.h"
-#include <filesystem>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <iostream>
-#include <cmath>
-
-#define LOGERR() std::cout << glGetError() << std::endl;
 
 class Mesh 
 {
 public:
 	Mesh(
 		float* vertices, 
-		unsigned int* indices, 
-		size_t numOfVertices, 
-		size_t numOfIndices,
-		bool useEBO);
+		size_t numOfVertices);
 	~Mesh();
 
 	const unsigned int VertexCount() const { return m_numOfVertices; }
-	const unsigned int IndexCount() const { return m_numOfIndices; }
-
 	const unsigned int GetVAO() const { return VAO; }
 	
-	const bool indexed() const { return m_useEBO; }
-
 private:
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 
 	float* m_vertices;
-	unsigned int* m_indices;
-
 	unsigned int m_numOfVertices;
-	unsigned int m_numOfIndices;
-
-	bool m_useEBO;
 };
